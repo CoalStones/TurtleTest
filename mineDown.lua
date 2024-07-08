f1dd439fac98fb7de53ae7e305fd7c7de9f9f55f
CoalStones <123333323+CoalStones@users.noreply.github.com>
2024-07-08 19:09:45 -0400

    Program to make a turtle mine out a section

---
+++ mineDown.lua
@@ -0,0 +1,48 @@
+-- taken from ItsJustChris on YT
+
+function checkDirection(direction)
+    if direction == true then
+        turtle.turnRight()
+    else
+        turtle.turnLeft()
+    end
+end
+ 
+direction = true
+while (level < 40) do
+    a = 0
+    repeat
+        i = 0
+        repeat
+            turtle.dig()
+            turtle.forward()
+            i = i + 1
+        until (i > 4)
+        if a < 5 then
+            checkDirection(direction)
+            turtle.dig()
+            turtle.forward()
+            checkDirection(direction)
+            if direction == true then
+                direction = false
+            else
+                direction = true
+            end
+        else
+            print("Stopped")
+        end
+        a = a + 1
+    until (a > 5)
+    turtle.turnRight()
+    j = 0
+    repeat
+        turtle.forward()
+        j = j + 1
+        print(j)
+    until (j >= 5)
+    turtle.turnRight()
+    turtle.digDown()
+    turtle.down()
+    direction = true
+    level += 1
+end
